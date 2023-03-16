b217199a9b51ce1f0e6f32c8a8a1939287c82c31
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-03-16 13:14:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import classes from './App.module.css'
+
+const App = () => {
+  return (
+      <main className={classes.root}>
+        <header className={classes.header}>
+          <h1>Welcome to yet another Tic-Tac-Toe Game!</h1>
+        </header>
+      </main>
+  )
+}
+
+export default App
